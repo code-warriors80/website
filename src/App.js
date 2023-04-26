949e46bfef004fb15ba20ecb10bf47456dc4601b
youngsavage-codes <117549855+youngsavage-codes@users.noreply.github.com>
2023-04-26 09:05:20 +0100

    fgf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Landing from './Components/Landing';
 
 function App() {
   return (
     <div className="App">
-
+        <Landing />
     </div>
   );
 }
---
+++ src/Components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className=''>
+               <h1>mmmm</h1>
+    </div>
+  )
+}
+
+export default Landing
