a02ca92cad4e06058774b0e8b00edc8d4c68a3c6
unknown <codewarriors02@gmail.com>
2023-04-26 10:53:36 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Landing from './Components/Landing';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+        <Navbar/>
         <Landing />
     </div>
   );
