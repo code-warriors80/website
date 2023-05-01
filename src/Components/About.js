753f0528a4ee3f33491f19beb06657bfec3d12f2
Sparrowthedev <sparrowthedev@gmail.com>
2023-05-01 21:42:10 +0100

    indented the following files

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,46 +1,73 @@
-import React from 'react'
-import robot from '../images/hero-shape-2.webp';
-import team from '../images/head.png'
-import { Zoom } from 'react-reveal'
-import { aboutdet } from '../Routes/ServiceList';
+import React from "react";
+import robot from "../images/hero-shape-2.webp";
+import team from "../images/head.png";
+import { Zoom } from "react-reveal";
+import { aboutdet } from "../Routes/ServiceList";
 
 const About = () => {
   return (
-    <section className='lg:mb-16'>
-            <Zoom><h2 className='heading text-center mt-16 lg:mt-0 text-3xl lg:text-5xl'><img src={robot} alt='' className='animate-bounce w-10 lg:w-16' />About <span>Our Team</span></h2></Zoom>
-            <Zoom><p className='heading-p mt-5 px-5 text-center text-sm mb-10 lg:mb-0 lg:text-sm font-thin lg:pb-10 text-gray-400'>What Makes us unique and why should you consider using our services</p></Zoom>
+    <section className="lg:mb-16">
+      <Zoom>
+        <h2 className="heading text-center mt-16 lg:mt-0 text-3xl lg:text-5xl">
+          <img src={robot} alt="" className="animate-bounce w-10 lg:w-16" />
+          About <span>Our Team</span>
+        </h2>
+      </Zoom>
+      <Zoom>
+        <p className="heading-p mt-5 px-5 text-center text-sm mb-10 lg:mb-0 lg:text-sm font-thin lg:pb-10 text-gray-400">
+          What Makes us unique and why should you consider using our services
+        </p>
+      </Zoom>
 
-              <div>
-                <div className='lg:flex lg:items-center lg:justify-between lg:flex-wrap lg:px-40 lg:py-14 px-5'>
-                    <Zoom><img src={team} alt='' className='w-full lg:w-4/12 py-5 lg:py-5' /></Zoom>
-                    <Zoom><div className='boost-text w-full lg:w-6/12'>
-                        <h3 className='text-gray-900 text-center lg:text-left font-extrabold text-4xl lg:text-5xl'>Offering The Best IT Services To  <span>Clients</span></h3>
-                        <p className='mt-5 text-gray-700 text-center lg:text-left lg:text-lg'>
-                            Like any great agency, we are only as good as the result we deliver of our recent work.
-                            Our developers are commited to maintaining the highest web standards so that your site will withstand the test of time
-                        </p>
-                        <div className='mt-4 flex justify-center lg:justify-start pb-10 lg:py-5'>
-                            <a href='/' className='tracking-wider font-semibold text-sm uppercase inline-block rounded-lg px-5 py-3 bg-indigo-500 text-white shadow-lg hover:animate-bounce delay-100'> Learn More</a>
-                        </div>
-                    </div></Zoom>
-                </div>
+      <div>
+        <div className="lg:flex lg:items-center lg:justify-between lg:flex-wrap lg:px-40 lg:py-14 px-5">
+          <Zoom>
+            <img src={team} alt="" className="w-full lg:w-4/12 py-5 lg:py-5" />
+          </Zoom>
+          <Zoom>
+            <div className="boost-text w-full lg:w-6/12">
+              <h3 className="text-gray-900 text-center lg:text-left font-extrabold text-4xl lg:text-5xl">
+                Offering The Best IT Services To <span>Clients</span>
+              </h3>
+              <p className="mt-5 text-gray-700 text-center lg:text-left lg:text-lg">
+                Like any great agency, we are only as good as the result we
+                deliver of our recent work. Our developers are commited to
+                maintaining the highest web standards so that your site will
+                withstand the test of time
+              </p>
+              <div className="mt-4 flex justify-center lg:justify-start pb-10 lg:py-5">
+                <a
+                  href="/"
+                  className="tracking-wider font-semibold text-sm uppercase inline-block rounded-lg px-5 py-3 bg-indigo-500 text-white shadow-lg hover:animate-bounce delay-100"
+                >
+                  {" "}
+                  Learn More
+                </a>
               </div>
-
-          <div className='about-container lg:flex lg:items-center lg:justify-center'>
-                    <div className='about-parent lg:flex px-5 lg:items-center lg:justify-around lg:w-10/12'>
-                      {aboutdet.map((abt, i) => {
-                        return (<Zoom key={i}>
-                            <div className='about-box mt-5 lg:mt-5 shadow-lg bg-red-300 lg:w-3/12 lg:mx-7 text-center py-10 px-7'>
-                                <div class='flex align-center justify-center mb-3'><img src={aboutdet[i].img} alt='' /></div>
-                                <h3>{aboutdet[i].title}</h3>
-                                <p className='mt-3'>{aboutdet[i].description}</p>
-                            </div>
-                      </Zoom>)
-                      })}
             </div>
+          </Zoom>
+        </div>
+      </div>
+
+      <div className="about-container lg:flex lg:items-center lg:justify-center">
+        <div className="about-parent lg:flex px-5 lg:items-center lg:justify-around lg:w-10/12">
+          {aboutdet.map((abt, i) => {
+            return (
+              <Zoom key={i}>
+                <div className="about-box mt-5 lg:mt-5 shadow-lg bg-red-300 lg:w-3/12 lg:mx-7 text-center py-10 px-7">
+                  <div class="flex align-center justify-center mb-3">
+                    <img src={aboutdet[i].img} alt="" />
+                  </div>
+                  <h3>{aboutdet[i].title}</h3>
+                  <p className="mt-3">{aboutdet[i].description}</p>
+                </div>
+              </Zoom>
+            );
+          })}
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,42 +9,97 @@ import { Zoom } from "react-reveal";
 const Contact = () => {
   return (
     <div className="align-middle justify-center lg:pt-28">
-          <Zoom><h2 className='heading text-center mt-16 lg:mt-0 text-3xl lg:text-5xl'><img src={robot} alt='' className='animate-bounce w-10 lg:w-16' />About <span>Our Team</span></h2></Zoom>
-          <Zoom><p className='heading-p mt-5 px-5 text-center text-sm mb-10 lg:mb-0 lg:text-sm font-thin lg:pb-10 text-gray-400'>What Makes us unique and why should you consider using our services</p></Zoom>
-          
-          <div className='hidden lg:flex lg:items-center lg:justify-center'>
-              <div className='reason-parent w-11/12 lg:w-7/12 flex items-center justify-between lg:justify-between py-10'>
-                  <Zoom><div className='lg:flex lg:items-center lg:justify-center'><div className='flex items-center justify-center '><img src={location} alt='' className='w-8 lg:w-5/12 md:w-2/6'/></div> <p className='text-sm font-thin mt-2 lg:font-bold lg:mt-0'>85 Howord St, Toronto</p></div></Zoom>
-                  <Zoom><div className='lg:flex lg:items-center lg:justify-center'><div className='flex items-center justify-center'><img src={chat} alt='' className='w-8 lg:w-5/12 md:w-2/6' /></div> <p className='text-sm font-tin mt-2 lg:font-bold lg:mt-0'> Codewarriors02@gmail.com</p></div></Zoom>
-                  <Zoom><div className='lg:flex lg:items-center lg:justify-center'><div className='flex items-center justify-center'><img src={user} alt='' className='w-8 lg:w-5/12 md:w-2/6' /></div>  <p className='text-sm font-thin lg:font-bold mt-3 lg:mt-0'> (+234) 07056882004</p></div></Zoom>
-              </div>
-          </div>
+      <Zoom>
+        <h2 className="heading text-center mt-16 lg:mt-0 text-3xl lg:text-5xl">
+          <img src={robot} alt="" className="animate-bounce w-10 lg:w-16" />
+          About <span>Our Team</span>
+        </h2>
+      </Zoom>
+      <Zoom>
+        <p className="heading-p mt-5 px-5 text-center text-sm mb-10 lg:mb-0 lg:text-sm font-thin lg:pb-10 text-gray-400">
+          What Makes us unique and why should you consider using our services
+        </p>
+      </Zoom>
 
-        <div className="lg:flex lg:items-center lg:justify-center lg:py-20 pb-10">
-            <div className="lg:flex lg:items-center lg:justify-around lg:w-10/12">
-              <Zoom>
-                  <img src={world} alt="" className="w-5/12 hidden lg:block"/>
-              </Zoom>
-              <Zoom>
-                  <form className="contact-form w-full lg:w-5/12 px-5">
-                      <div className="w-full mb-5 flex items-center justify-between flex-wrap">
-                            <input type="text" placeholder="Name" className="mb-5 lg:mb-0 w-[100%] lg:w-[48%] h-[60px] px-3 border-solid rounded-2xl outline-none"/>
-                            <input type="text" placeholder="Email" className="w-[100%] lg:w-[48%] h-[60px] px-3 rounded-2xl outline-none"/>
-                      </div>
-
-                      <div className="flex items-center justify-center mb-5">
-                          <input type="text" placeholder="Subject" className="w-full h-[60px] px-3 rounded-2xl outline-none"/>
-                      </div>
-
-                      <div className="flex items-center justify-center mb-5">
-                            <textarea placeholder='Type Your Message' className="w-full h-[250px] lg:h-[300px] p-3 rounded-2xl outline-none"></textarea>
-                      </div>
-
-                      <button className="'tracking-wider font-semibold text-sm uppercase inline-block rounded-lg px-5 py-3 bg-indigo-500 text-white shadow-lg hover:animate-bounce delay-100">Send Message</button>
-                  </form>
-                </Zoom>
+      <div className="hidden lg:flex lg:items-center lg:justify-center">
+        <div className="reason-parent w-11/12 lg:w-7/12 flex items-center justify-between lg:justify-between py-10">
+          <Zoom>
+            <div className="lg:flex lg:items-center lg:justify-center">
+              <div className="flex items-center justify-center ">
+                <img src={location} alt="" className="w-8 lg:w-5/12 md:w-2/6" />
+              </div>{" "}
+              <p className="text-sm font-thin mt-2 lg:font-bold lg:mt-0">
+                85 Howord St, Toronto
+              </p>
             </div>
+          </Zoom>
+          <Zoom>
+            <div className="lg:flex lg:items-center lg:justify-center">
+              <div className="flex items-center justify-center">
+                <img src={chat} alt="" className="w-8 lg:w-5/12 md:w-2/6" />
+              </div>{" "}
+              <p className="text-sm font-tin mt-2 lg:font-bold lg:mt-0">
+                {" "}
+                Codewarriors02@gmail.com
+              </p>
+            </div>
+          </Zoom>
+          <Zoom>
+            <div className="lg:flex lg:items-center lg:justify-center">
+              <div className="flex items-center justify-center">
+                <img src={user} alt="" className="w-8 lg:w-5/12 md:w-2/6" />
+              </div>{" "}
+              <p className="text-sm font-thin lg:font-bold mt-3 lg:mt-0">
+                {" "}
+                (+234) 07056882004
+              </p>
+            </div>
+          </Zoom>
         </div>
+      </div>
+
+      <div className="lg:flex lg:items-center lg:justify-center lg:py-20 pb-10">
+        <div className="lg:flex lg:items-center lg:justify-around lg:w-10/12">
+          <Zoom>
+            <img src={world} alt="" className="w-5/12 hidden lg:block" />
+          </Zoom>
+          <Zoom>
+            <form className="contact-form w-full lg:w-5/12 px-5">
+              <div className="w-full mb-5 flex items-center justify-between flex-wrap">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="mb-5 lg:mb-0 w-[100%] lg:w-[48%] h-[60px] px-3 border-solid rounded-2xl outline-none"
+                />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="w-[100%] lg:w-[48%] h-[60px] px-3 rounded-2xl outline-none"
+                />
+              </div>
+
+              <div className="flex items-center justify-center mb-5">
+                <input
+                  type="text"
+                  placeholder="Subject"
+                  className="w-full h-[60px] px-3 rounded-2xl outline-none"
+                />
+              </div>
+
+              <div className="flex items-center justify-center mb-5">
+                <textarea
+                  placeholder="Type Your Message"
+                  className="w-full h-[250px] lg:h-[300px] p-3 rounded-2xl outline-none"
+                ></textarea>
+              </div>
+
+              <button className="'tracking-wider font-semibold text-sm uppercase inline-block rounded-lg px-5 py-3 bg-indigo-500 text-white shadow-lg hover:animate-bounce delay-100">
+                Send Message
+              </button>
+            </form>
+          </Zoom>
+        </div>
+      </div>
     </div>
   );
 };
